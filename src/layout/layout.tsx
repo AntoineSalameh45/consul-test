503375c50b6af88934194a579a197617300a21d8
BiG A <antoine.salameh.45@gmail.com>
2026-02-05 18:01:25 +0200

    feat: implement BackToTopButton and ScrollToTop components

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,19 +1,22 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/organisms/Navbar";
-import { BackToTopButton } from "../components/atoms/BackToTopButton";
+import { BackToTopButton } from "../components/utils/BackToTopButton";
 import { ArrowUpIcon } from "../assets/icons";
+import { ScrollToTop } from "../components/utils/ScrollToTop";
 
 const SharedLayout = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
+      
+      <ScrollToTop />
 
       <main className="pt-24 bg-blue-950">
         <Outlet />
       </main>
       
       <BackToTopButton
-        icon={<ArrowUpIcon className="w-15 h-15" />}
+        icon={<ArrowUpIcon className="w-12 h-12" />}
         showAfter={400}
       />
     </div>
